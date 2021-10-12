77a36f9ef7a729726e82e9d69ac7b206cd4ed8d9
EC2 Default User <ec2-user@ip-172-31-14-42.us-east-2.compute.internal>
2021-10-12 16:55:17 +0000

    ヘッダー、フッターの実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def full_title(page_title = '')  # full_titleメソッドを定義
+    base_title = 'Keyword-profit'
+    if page_title.blank?
+      base_title  # トップページはタイトル「Keyword-profit」
+    end
+  end
 end
