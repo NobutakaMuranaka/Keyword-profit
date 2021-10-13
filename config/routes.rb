bb23233813ff949860a7124555837ac6ea613358
NobutakaMuranaka <chanayataka2324@gmail.com>
2021-10-13 15:37:02 +0000

    userコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  get :signup,       to: 'users#new'
+  resources :users
 end
