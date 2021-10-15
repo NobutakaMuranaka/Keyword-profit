ee79990fe2bc66b99588379715956c7b63674367
NobutakaMuranaka <chanayataka2324@gmail.com>
2021-10-15 03:47:41 +0000

    一覧表示のUIを整える・seedでサンプル表示

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,5 +1,6 @@
 class Keyword < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
   validates :volume, :goal_pv, :goal_cvr, :goal_price, :goal_profit, :goal_rank, presence: true
   validates :description, length: { maximum: 200 }
   validates :query, presence: true, length: { maximum: 30 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ユーザー
+User.create!(
+  [
+    {
+      name:  "山田 良子",
+      email: "yamada@example.com",
+      password:              "foobar",
+      password_confirmation: "foobar",
+    }
+  ]
+)
+
+10.times do |n|
+  Keyword.create!(
+    query: "タブレット学習 デメリット",
+    volume: 400,
+    goal_rank: 1,
+    goal_pv: 100,
+    goal_cvr: 1.0,
+    goal_price: 1000,
+    goal_profit: 1000,
+    description: "競合性そこそこ",
+    user_id: 1)
+end
